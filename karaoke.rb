f5604d62eadf286b6a6087709314e505b4f35c85
techno-tanoC <techno.tanoc@gmail.com>
2014-06-18 01:44:40 +0900

    refactor

--- karaoke.rb
+++ karaoke.rb
@@ -8,22 +8,22 @@ class KaraokeMachine
   end
 
   def transpose(amount)
-#    @melody.gsub(/[A-G]#?/, &proc_scala(amount))
-    @melody.gsub(/[A-G]#?/, &(KEYBOARD._.(:index) >> (amount % 12)._.(:+) >> method(:flip_mod) >> KEYBOARD._.(:at)))
+    flip_mod = -> num { num % 12 }
+    @melody.gsub(/[A-G]#?/, &KEYBOARD._.(:index) >> (amount % 12)._.(:+) >> flip_mod >> KEYBOARD._.(:at))
   end
 
-  private
-
-#  def proc_scala(amount)
-#    -> scala { (KEYBOARD._.(:index) >= (amount % 12)._.(:+) >= method(:flip_mod) >= KEYBOARD._.(:at)).(scala) || scala }
-#  end
-
-  def flip_mod(num)
-    num % 12
+  #lambda_driver使わないバージョン
+  def not_functional(amount)
+    @melody.gsub(/[A-G]#?/) {|exp|
+      idx = KEYBOARD.index(exp)
+      mod = (amount % 12) + idx
+      ans_idx = mod % 12
+      KEYBOARD[ans_idx]
+    }
   end
 end
 
 melody = "C D E F |E D C   |E F G A |G F E   |C   C   |C   C   |CCDDEEFF|E D C   "
 melody2 = "F# G# A# B |A# G# F#   |A# B C# D# |C# B A#   |F#   F#   |F#   F#   |F#F#G#G#A#A#BB|A# G# F#   "
 karaoke = KaraokeMachine.new(melody)
-p karaoke.transpose(2)
+p karaoke.transpose(-1)
