9b0c4963cb759d8ed185a5e2c8d5127a5e333d80
techno-tanoC <techno.tanoc@gmail.com>
2014-06-18 01:34:17 +0900

    refactor

--- karaoke.rb
+++ karaoke.rb
@@ -1,11 +1,29 @@
 require 'lambda_driver'
 
 class KaraokeMachine
+  KEYBOARD = %w(C C# D D# E F F# G G# A A# B).freeze
+
   def initialize(melody)
+    @melody = melody
+  end
+
+  def transpose(amount)
+#    @melody.gsub(/[A-G]#?/, &proc_scala(amount))
+    @melody.gsub(/[A-G]#?/, &(KEYBOARD._.(:index) >> (amount % 12)._.(:+) >> method(:flip_mod) >> KEYBOARD._.(:at)))
+  end
+
+  private
+
+#  def proc_scala(amount)
+#    -> scala { (KEYBOARD._.(:index) >= (amount % 12)._.(:+) >= method(:flip_mod) >= KEYBOARD._.(:at)).(scala) || scala }
+#  end
+
+  def flip_mod(num)
+    num % 12
   end
 end
 
 melody = "C D E F |E D C   |E F G A |G F E   |C   C   |C   C   |CCDDEEFF|E D C   "
 melody2 = "F# G# A# B |A# G# F#   |A# B C# D# |C# B A#   |F#   F#   |F#   F#   |F#F#G#G#A#A#BB|A# G# F#   "
 karaoke = KaraokeMachine.new(melody)
-p karaoke.transpose(-1)
+p karaoke.transpose(2)
